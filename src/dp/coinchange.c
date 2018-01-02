65bb6094555c3911f120aebd57524c2ca7db7eee
Shrek Zhou <shrek.zhou@livenation.com>
2018-01-02 11:57:50 -0800

    coin change issue

---
+++ src/dp/coinchange.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+Input:
+The first line contains an integer 'T' denoting the total number of test cases. In each test cases, the first line contains an integer 'M' denoting the size of array. The second line contains M space-separated integers A1, A2, ..., AN denoting the elements of the array. The third line contains an integer 'N' denoting the cents.
+
+Output:
+Print number of possible ways to make change for N cents.
+*/
+
+int main(int argc, char const *argv[])
+{
+	printf("hello world\n");
+	return 0;
+}
